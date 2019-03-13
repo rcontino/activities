491b40edd2d49293fbe7cc112b6c865b8227a72b
russ <racontino@gmail.com>
2019-03-13 22:37:26 +0000

    first commit

---
+++ src/test/java/com/nemo/activities/ActivitiesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nemo.activities;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ActivitiesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
